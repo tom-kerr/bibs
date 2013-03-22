940b2f8edc2ad71284caec20796fac6552e71f99
reklaklislaw <thomkerr@gmail.com>
2013-03-21 23:45:59 -0400

    add dplav1 and add apis to openlibrary. improve bibs.py

--- bibs.py
+++ bibs.py
@@ -40,7 +40,7 @@ class Bibs:
             query_string = self.format_as_json(query, search_source, api)
         elif input_type == 'key_value':
             query_string = self.format_as_key_value(query, search_source, api)
-        elif input_type == 'lazy':
+        elif input_type == 'lazy_key_value':
             query_string = self.format_lazy(query, search_source, api)
         #print query_string
         #return
@@ -48,6 +48,20 @@ class Bibs:
         results = request.read()
         return json.loads(results)
         #return results
+
+
+    def get_param(self, element, params):
+        for param in params:
+            if type(param) == dict:
+                for key, value  in param.items():
+                    match = re.search('(?i)'+element, key)
+                    if match:
+                        return param
+            else:
+                match = re.search('(?i)'+element, param)
+                if match:
+                    return param
+        return False
 
 
     def format_lazy(self, query, source, api):
@@ -59,7 +73,8 @@ class Bibs:
         else:
             query_string = ''
             for num in range(0, len(elements), 2):
-                query_string += str(elements[num]) + str(option_bind_char) + urllib2.quote(elements[num+1])
+                key, value = elements[num], elements[num + 1]
+                query_string += str(key) + str(option_bind_char) + urllib2.quote(value)
                 if num != len(elements) - 2:
                     query_string += str(option_chain_char)
         return source['url'] + source['api'][api]['path'] + query_string
@@ -73,11 +88,15 @@ class Bibs:
         option_bind_char, option_chain_char = source['api'][api]['input']['option_bind_chain']
         query_string = ''
         for num in range(0, len(elements), 2):
-            key = elements[num]
-            if key in source['api'][api]['input']['params']:
-                query_string += str(key) + str(param_bind_char) + str(elements[num+1]) + str(param_chain_char)
-            elif key in source['api'][api]['input']['options']:
-                query_string += str(option_chain_char) + str(key) + str(option_bind_char) + str(elements[num+1])
+            key, value = elements[num], elements[num + 1]
+            param = self.get_param(key, source['api'][api]['input']['params'])
+            if param:
+                query_string += str(param) + str(param_bind_char) + urllib2.quote(value) + str(param_chain_char)
+                continue
+            param = self.get_param(key, source['api'][api]['input']['options'])
+            if param:
+                query_string += str(option_chain_char) + str(param) + str(option_bind_char) + urllib2.quote(value)
+                continue
             else:
                 raise Exception('Invalid query: \''+str(key)+'\' unknown parameter.')
         return source['url'] + source['api'][api]['path'] + query_string
@@ -85,8 +104,8 @@ class Bibs:
 
     def format_as_json(self, query, source, api):
         elements = query.split(':')
-        qtype = elements[0]
-        if qtype not in source['api'][api]['input']['params']:
+        qtype = self.get_param(elements[0], source['api'][api]['input']['params'])
+        if not qtype:
             raise Exception('Invalid query: \''+str(qtype)+'\' unknown parameter.')
         elements = elements[1:]
         if len(elements)%2!=0:
@@ -95,8 +114,16 @@ class Bibs:
         qstrings = []
         property_items = []
         for num in range(0, len(elements), 2):
-            property_items.append(self.assign_property(elements[num], elements[num+1], 
-                                                       source['api'][api]['input']['params'][qtype]))            
+            key, value = elements[num], elements[num+1]
+            param = self.get_param(key, source['api'][api]['input']['params'][qtype])
+            print param
+            if type(param) == tuple:
+                property_items.append((param, urllib2.quote(value)))
+            elif type(param) == dict:
+                param_dict = self.assign_params(param, value)
+                property_items.append(param_dict)
+            else:
+                raise Exception('Invalid query: \''+qproperty+'\' unknown property')
         query_string = "{\"type\":\"\/type\/" + qtype + "\""
         for item in property_items:
             if type(item) == tuple:
@@ -107,16 +134,12 @@ class Bibs:
         return query_string
 
 
-    def assign_property(self, qproperty, qstring, types):
-        for p in types:
-            if p == str(qproperty):
-                return (qproperty, qstring)
-            elif qproperty in p:
-                item = p
-                for entry, chain in item[qproperty].items():
-                    chain['key'] = '/' + qproperty + '/' + qstring
-                return item
-        raise Exception('Invalid query: \''+qproperty+'\' unknown property')
+    def assign_params(self, param_dict, value):
+        for param, dictionary in param_dict.items():
+            for key, pair in dictionary.items():
+                pair['key'] = '/' + param + '/' + value
+        return param_dict
+        
 
 
         
